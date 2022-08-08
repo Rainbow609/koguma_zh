280fb7508b8224ce55d7da637e1e9727d80766d4
Andreas <andreas.everos@gmail.com>
2022-08-08 20:33:32 +0200

    Hide BottomNavigation when leaving root screens

--- app/src/main/java/me/ghostbear/koguma/ui/BottomNavigationBar.kt
+++ app/src/main/java/me/ghostbear/koguma/ui/BottomNavigationBar.kt
@@ -9,6 +9,9 @@
 package me.ghostbear.koguma.ui
 
 import androidx.annotation.StringRes
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Home
@@ -21,6 +24,7 @@ import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.res.stringResource
@@ -39,31 +43,36 @@ fun BottomNavigationBar(
 
     val backgroundColor = NavigationBarDefaults.containerColor
 
-    NavigationBar(
-        modifier = Modifier
-            .drawBehind { drawRect(backgroundColor) }
-            .navigationBarsPadding()
+    AnimatedVisibility(
+        visible = currentDestination?.route in Route.rootScreens.map(Route::route),
+        enter = expandVertically(expandFrom = Alignment.Bottom),
+        exit = shrinkVertically(shrinkTowards = Alignment.Bottom)
     ) {
-        Route.rootScreens.forEach { screen ->
-            NavigationBarItem(
-                selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
-                onClick = {
-                    navController.navigate(screen.route) {
-                        popUpTo(navController.graph.findStartDestination().id) {
-                            saveState = true
+        NavigationBar(
+            modifier = Modifier
+                .drawBehind { drawRect(backgroundColor) }
+                .navigationBarsPadding()
+        ) {
+            Route.rootScreens.forEach { screen ->
+                NavigationBarItem(
+                    selected = currentDestination?.hierarchy?.any { it.route == screen.route } == true,
+                    onClick = {
+                        navController.navigate(screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
+                    },
+                    icon = screen.icon,
+                    label = {
+                        Text(text = stringResource(id = screen.textId))
                     }
-                },
-                icon = screen.icon,
-                label = {
-                    Text(text = stringResource(id = screen.textId))
-                }
-            )
+                )
+            }
         }
     }
-
 }
 
 sealed class Route(val route: String, @StringRes val textId: Int, val icon: @Composable () -> Unit) {
